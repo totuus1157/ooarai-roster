22885d963c9298a5c3475f142f5688ce3c1af72a
totuus1157 <takahatam@icloud.com>
2020-10-16 17:02:35 +0900

    studentsテーブル全体を表示

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const db = require('../models/index');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  const stus = await db.Student.findAll();
+  res.render('index', { title: '大洗女子学園戦車道履修者名簿', stus });
 });
 
 module.exports = router;
