de286f5916316957f89eec11e0fcccf3d1685b24
totuus1157 <takahatam@icloud.com>
2020-10-20 11:17:52 +0900

    レコードの新規作成ページ作成

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,4 +8,26 @@ router.get('/', async function(req, res, next) {
   res.render('index', { title: '大洗女子学園戦車道履修者名簿', stus });
 });
 
+router.get('/add', (req, res, next) => {
+  const data = { title: '新規追加' }
+  res.render('add', data);
+});
+
+router.post('/add', (req, res, next) => {
+  db.sequelize.sync()
+    .then(() => db.Student.create({
+      name: req.body.name,
+      name_kana: req.body.name_kana,
+      year: req.body.year,
+      height: req.body.height,
+      team: req.body.team,
+      role: req.body.role,
+      leader: req.body.leader,
+      nickname: req.body.nickname
+    }))
+    .then(stus => {
+      res.redirect('/');
+    });
+});
+
 module.exports = router;
