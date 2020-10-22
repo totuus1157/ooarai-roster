c7b8bee73accc9477a1bb8b71708387106e2a14a
totuus1157 <takahatam@icloud.com>
2020-10-22 10:28:37 +0900

    テーブル表示のコードを教本準拠に変更

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,14 @@ const router = express.Router();
 const db = require('../models/index');
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
-  const stus = await db.Student.findAll();
-  res.render('index', { title: '大洗女子学園戦車道履修者名簿', stus });
+router.get('/', (req, res, next) => {
+  db.Student.findAll().then(stus => {
+    const data = {
+      title: '大洗女子学園戦車道履修者名簿',
+      content: stus
+    }
+    res.render('index', data);
+  });
 });
 
 router.get('/add', (req, res, next) => {
